7d8d10fac7c3304a8b117789eec08d0d0e14ba08
Manish <rajakamanishsingh@gmail.com>
2021-05-07 00:14:19 -0700

    commit

--- commandline1.java
+++ commandline1.java
@@ -3,9 +3,5 @@ public class commandline1 {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         System.out.println("a+b=" + (a + b));
-
     }
-
-    public static void main
-
 }
